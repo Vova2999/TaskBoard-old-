be2b824bc9ff52dbcc2841fa280a8c1bbe0b5604
Vova2999 <Vova2999@gmail.com>
2017-05-27 14:28:29 +0400

    Server: Добавил DatabaseAuthorizer и модуль Ninject

---
+++ TaskBoard.Server/TaskBoard.Server.Database/Models/DatabaseAuthorizer.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using TaskBoard.Common.Database;
+using TaskBoard.Server.Database.Tables;
+
+namespace TaskBoard.Server.Database.Models {
+	public class DatabaseAuthorizer : IDatabaseAuthorizer {
+		private readonly ModelDatabase modelDatabase;
+
+		public DatabaseAuthorizer(ModelDatabase modelDatabase) {
+			this.modelDatabase = modelDatabase;
+
+			if (!modelDatabase.Users.Any())
+				AddFirstUser();
+		}
+		private void AddFirstUser() {
+			modelDatabase.Users.Add(new User {
+				Login = "login",
+				Password = "password",
+				AccessType = int.MaxValue
+			});
+			modelDatabase.SaveChanges();
+		}
+
+		public bool UserIsExist(string login, string password) {
+			return modelDatabase.Users.FirstOrDefault(user => user.Login == login && user.Password == password) != null;
+		}
+		public bool AccessIsAllowed(string login, string password, int requestedAccessType) {
+			var foundUser = modelDatabase.Users.First(user => user.Login == login && user.Password == password);
+			return -(foundUser.AccessType | (-requestedAccessType - 1)) - 1 == 0;
+		}
+	}
+}
---
+++ TaskBoard.Server/TaskBoard.Server.Database/NinjectDatabaseModule.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using Ninject.Extensions.Conventions;
+using Ninject.Modules;
+using TaskBoard.Server.Database.Models;
+
+namespace TaskBoard.Server.Database {
+	public class NinjectDatabaseModule : NinjectModule {
+		public override void Load() {
+			System.Data.Entity.Database.SetInitializer(new DropCreateDatabaseIfModelChanges<ModelDatabase>());
+
+			Kernel?.Bind<ModelDatabase>().ToSelf().InSingletonScope();
+			Kernel?.Bind(c => c.FromThisAssembly().SelectAllClasses().BindAllInterfaces().Configure(y => y.InSingletonScope()));
+		}
+	}
+}
