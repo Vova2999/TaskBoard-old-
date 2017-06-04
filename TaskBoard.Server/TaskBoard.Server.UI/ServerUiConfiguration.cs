231a03336b893a1eeabab6a546e49389d79f2c82
Vova2999 <Vova2999@gmail.com>
2017-06-04 18:46:15 +0400

    Server: Поправил опечатку

--- TaskBoard.Server/TaskBoard.Server.UI/ServerUiConfiguration.cs
+++ TaskBoard.Server/TaskBoard.Server.UI/ServerUiConfiguration.cs
@@ -2,7 +2,7 @@
 
 namespace TaskBoard.Server.UI {
 	public class ServerUiConfiguration : ConfigurationFile<ServerUiConfiguration> {
-		protected override string ConfigurationFileName => "GraduateWork.Server.UI.Configuration.xml";
+		protected override string ConfigurationFileName => "TaskBoard.Server.UI.Configuration.xml";
 
 		public string UserLogin { get; set; }
 		public string UserPassword { get; set; }
